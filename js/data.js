be7084a6dfb33a047262aca7237cc29a4c3c31d3
IrynaRd <iryna.rudnytska.ir.2024@lpnu.ua>
2025-09-30 01:14:22 +0300

    adding 3 lab

---
+++ js/data.js
@@ -0,0 +1,52 @@
+export const data = [
+    {
+        id: 1,
+        title: "Harry Potter",
+        author: "J.K. Rowling",
+        price: 1000,
+        pages: 350,
+        description: "Fantasy novel about a wizard boy",
+        image: "images/Open_book_nae_02.svg"
+    },
+
+    {
+        id: 2,
+        title: "Harry Potter 2",
+        author: "J.K. Rowling",
+        price: 500,
+        pages: 400,
+        description: "Fantasy novel about a wizard boy",
+        image: "images/Open_book_nae_02.svg"
+    },
+
+    {
+        id: 3,
+        title: "Harry Potter 3",
+        author: "J.K. Rowling",
+        price: 300,
+        pages: 500,
+        description: "Fantasy novel about a wizard boy",
+        image: "images/Open_book_nae_02.svg"
+    },
+
+    {
+        id: 4,
+        title: "Harry Potter 4",
+        author: "J.K. Rowling",
+        price: 200,
+        pages: 600,
+        description: "Fantasy novel about a wizard boy",
+        image: "images/Open_book_nae_02.svg"
+    },
+
+    {
+        id: 5,
+        title: "Harry Potter 5",
+        author: "J.K. Rowling",
+        price: 250,
+        pages: 700,
+        description: "Fantasy novel about a wizard boy",
+        image: "images/Open_book_nae_02.svg"
+    }
+
+];
--- js/main.js
+++ js/main.js
@@ -1,11 +1,66 @@
-const OPEN_CLASSNAME = "open";
+import { data } from "./data.js";
 
-const navLinks = document.getElementById("nav-links");
+const searchButton = document.getElementById("search");
+const searchInput = document.getElementById("searchInput");
+const reset_searchButton = document.getElementById("reset__search");
+const sortButton = document.getElementById("sort");
+const countButton = document.getElementById("count");
+const bookList = document.getElementById("bookList");
+const totalExpences = document.getElementById("total_expences");
 
-function toggleMenu() {
-    if (navLinks.classList.contains(OPEN_CLASSNAME)) {
-        navLinks.classList.remove(OPEN_CLASSNAME);
-    } else {
-        navLinks.classList.add(OPEN_CLASSNAME);
-    }
+const itemTemplate = ({ id, title, author, price, pages, description }) => `
+<li id="${id}" class="card mb-3 item-card" draggable="true">
+    <img
+        src="images/Open_book_nae_02.svg"
+        class="item-container__image card-img-top" alt="card">
+    <div class="card-body">
+        <h5 class="card-title">${title}</h5>
+        <p class="card-author"><strong>Author:</strong> ${author}</p>
+        <p class="card-price"><strong>Price:</strong> $${price}</p>
+        <p class="card-pages"><strong>Pages:</strong> ${pages}</p>
+        <p class="card-description">${description}</p>
+
+        <div class="d-flex justify-content-between mt-3">
+            <button class="btn btn-warning btn-sm edit-btn">Edit</button>
+            <button class="btn btn-danger btn-sm remove-btn">Remove</button>
+        </div>
+    </div>
+</li>`;
+
+function renderBooks(array) {
+    bookList.innerHTML = "";
+    array.forEach(book => {
+        bookList.insertAdjacentHTML("beforeend", itemTemplate(book));
+        const bookElement = bookList.lastElementChild;
+        const removeBtn = bookElement.querySelector(".remove-btn");
+        removeBtn.addEventListener("click", () => {
+            bookElement.remove();
+        });
+    });
 }
+
+renderBooks(data);
+
+searchButton.addEventListener("click", () => {
+    const foundBooks = data.filter(
+        (book) => book.title.toLowerCase() === searchInput.value.toLowerCase()
+    );
+    renderBooks(foundBooks);
+});
+
+reset_searchButton.addEventListener("click", () => {
+    renderBooks(data);
+    searchInput.value = "";
+});
+
+sortButton.addEventListener("click", () => {
+    data.sort((a, b) => a.price - b.price);
+    renderBooks(data);
+});
+
+countButton.addEventListener("click", () => {
+    const total_price = data.reduce((sum, book) =>
+        sum + book.price, 0);
+    totalExpences.textContent = total_price;
+    
+});
