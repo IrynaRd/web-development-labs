7d534468174f83e3ff725b001c02e9c3ef103ba2
IrynaRd <iryna.rudnytska.ir.2024@lpnu.ua>
2025-11-17 22:34:02 +0200

    adding lab 9

--- lab/src/containers/Catalog/Catalog.jsx
+++ lab/src/containers/Catalog/Catalog.jsx
@@ -1,73 +1,95 @@
-import React, {useState} from "react"
+import React, { useState, useEffect } from "react"
 import CardItem from "../CardItem/CardItem";
-import { books, GenreOptions, OriginOptions, CoverOptions } from "../../assets/data/data";
+import { GenreOptions, OriginOptions, CoverOptions } from "../../assets/data/data";
 import { CardWrapper, HeadWrapper, FilterWrapper } from "../Catalog/Catalog.styled";
 import Filter from "./Select/Select";
 import { Button } from './Catalog.styled';
 import Item from './Item/Item';
+import api from "./../api/books";
+import Loader from "./../api/Loader.styled";
+
 
 const Catalog = () => {
+
+    const [books, setBooks] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const fetchBooks = async (params = {}) => {
+        setLoading(true);
+        try {
+            const response = await api.get('/', { params: params });
+            setBooks(response.data);
+        } catch (err) {
+            if (err.response) {
+                console.log(err.response.data);
+                console.log(err.response.status);
+                console.log(err.response.headers);
+            } else {
+                console.log(`Error: ${err.message}`);
+            }
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchBooks();
+    }, [])
+
+
     const [selectedBookID, setSelectedBookID] = useState(null);
 
     const [search, setSearch] = useState("");
-    
 
     const [selectedGenre, setSelectedGenre] = useState(null);
     const [selectedOrigin, setSelectedOrigin] = useState(null);
     const [selectedCover, setSelectedCover] = useState(null);
 
-    const [appliedGenre, setAppliedGenre] = useState(null);
-    const [appliedOrigin, setAppliedOrigin] = useState(null);
-    const [appliedCover, setAppliedCover] = useState(null);
 
     const applyFilters = () => {
-        setAppliedGenre(selectedGenre);
-        setAppliedOrigin(selectedOrigin);
-        setAppliedCover(selectedCover);
-    };
+        const filters = {};
+        if (selectedGenre) {
+            filters.genre = selectedGenre;
+        }
 
-    const currentBooks = books.filter(book => {
-        const searched = search.toLowerCase().trim();
-        const searchMatch = (searched === '') ? true : (
-            book.title.toLowerCase().includes(searched) || 
-            book.author.toLowerCase().includes(searched) || 
-            book.text.toLowerCase().includes(searched)
-        );
+        if (selectedOrigin) {
+            filters.origin = selectedOrigin;
+        }
 
-        const genreMatch = !appliedGenre ? true : book.genre === appliedGenre;
-        const originMatch = !appliedOrigin ? true : book.origin === appliedOrigin;
-        const coverMatch = !appliedCover ? true : book.cover === appliedCover;
+        if (selectedCover) {
+            filters.cover = selectedCover;
+        }
+
+        if (search) {
+            filters.search = search;
+        }
         
-        return searchMatch && genreMatch && originMatch && coverMatch;
-    })
-
+        fetchBooks(filters);
+    };
 
     if (selectedBookID) {
         const selectedBook = books.find(book => book.id === selectedBookID);
         return (
-            <Item book={selectedBook} onGoBack={()=>setSelectedBookID(null)}></Item>
+            <Item book={selectedBook} onGoBack={() => setSelectedBookID(null)}></Item>
         )
-        
     }
-    
-
 
     return (
         <div>
             <HeadWrapper>
                 <FilterWrapper>
-                    <Filter placeholder = {"Genre"} options = {GenreOptions} onChange={value => setSelectedGenre(value)}/>
-                    <Filter placeholder = {"Origin"} options = {OriginOptions} onChange={value => setSelectedOrigin(value)}/>
-                    <Filter placeholder = {"Book Cover"} options = {CoverOptions} onChange={value => setSelectedCover(value)}/>
-                    <input type="text" placeholder="Search..." 
-                        onChange={(e) => setSearch(e.target.value)}/>
+                    <Filter placeholder={"Genre"} options={GenreOptions} onChange={value => setSelectedGenre(value)} />
+                    <Filter placeholder={"Origin"} options={OriginOptions} onChange={value => setSelectedOrigin(value)} />
+                    <Filter placeholder={"Book Cover"} options={CoverOptions} onChange={value => setSelectedCover(value)} />
+                    <input type="text" placeholder="Search..." value = {search}
+                        onChange={(e) => setSearch(e.target.value)} />
                 </FilterWrapper>
 
                 <Button onClick={applyFilters}>Apply</Button>
             </HeadWrapper>
 
+            {loading ? <Loader /> :
             <CardWrapper>
-                {currentBooks.map(({ title, author, text, image, price, id }) => (
+                {books.map(({ title, author, text, image, price, id }) => (
                     <CardItem
                         key={id}
                         title={title}
@@ -75,16 +97,14 @@ const Catalog = () => {
                         text={text}
                         imageSrc={image}
                         price={price}
-                        onShowMore={()=>setSelectedBookID(id)}
+                        onShowMore={() => setSelectedBookID(id)}
                     />
                 )
-
                 )}
             </CardWrapper>
-
+            }
         </div>
     )
 }
-
 
 export default Catalog;
